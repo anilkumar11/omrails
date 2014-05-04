8e92d772f68dab5c77508ed1dc55a24420f30e78
anil <anil.iitkgp@gmail.com>
2014-05-04 16:59:10 +0530

    Added Pins Associations

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,8 @@
 class Pin < ActiveRecord::Base
 	include ActiveModel::MassAssignmentSecurity
-	attr_accessible :description
+	attr_accessible :description, :user, :user
 	validates :description, presence: true
+	validates :user_id, presence: true
+
+	belongs_to :user
 end
